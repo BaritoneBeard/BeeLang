564fe892f500c2ce404cc6867ac602baf652a8e3
Nicholas Almeder <32600512+BaritoneBeard@users.noreply.github.com>
2021-12-13 17:47:43 -0500

    Added Bee.py

---
+++ Bee.py
@@ -0,0 +1,65 @@
+register = [0] * 10
+ptr = 0
+universal = 0
+
+Alphabet = {
+	0:'a',
+	1:'b',
+	2:'c',
+	3:'d',
+	4:'e',
+	5:'f',
+	6:'g',
+	7:'h',
+	8:'i',
+	9:'j',
+	10:'k',
+	11:'l',
+	12:'m',
+	13:'n',
+	14:'o',
+	15:'p',
+	16:'q',
+	17:'r',
+	18:'s',
+	19:'t',
+	20:'u',
+	21:'v',
+	22:'w',
+	23:'x',
+	24:'y',
+	25:'z',
+	-1:' '
+}
+def main():
+	global ptr, register
+	print("Enter 'bee' for help")
+	while(1):
+		beeString = str(input())
+		if(len(beeString) != 3):
+			raise ValueError('Input must have form "bee" ')
+		B1 = beeString[0]
+		B2 = beeString[1]
+		B3 = beeString[2]
+		#print(B1,B2,B3)
+		if beeString == "bEe":
+			ptr -= 1
+		elif beeString == "beE":
+			ptr += 1
+		elif beeString == "BeE":
+			register[ptr] += 1
+		elif beeString == "BEe":
+			register[ptr] -= 1
+		elif beeString == "Bee":
+			print(register[ptr])
+		elif beeString == "BEE":
+			print(register)
+			for i in range(len(register)):
+				print("", Alphabet[register[i]],end= " ")
+			print()
+		elif beeString == "bee":
+			print("\nB refers to current register, b refers to ptr location")
+			print("eE increments, Ee decrements")
+			print("BEE prints all registers, Bee prints current register\n")
+if __name__ == '__main__':
+	main()
